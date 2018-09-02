b3e928cab0ce8bd9bcbd14a7651faddd4264dfe4
dickiez <dickiezulkarnaen@gmail.com>
2018-09-02 21:00:22 +0700

    Menyelesaikan Masalah Orang Dengan Ikhlas

---
+++ app/src/main/java/com/example/dickiez/pdfviewer/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.dickiez.pdfviewer;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    PDFView pdf;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pdf = (PDFView)findViewById(R.id.pdfviewer);
+        //pdf.fromAsset("sample.pdf").load();
+
+        new RetrievePDFStream().execute("http://www.ancestralauthor.com/download/sample.pdf");
+
+    }
+
+    class RetrievePDFStream extends AsyncTask<String, Void, InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+            } catch (IOException e) {
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdf.fromStream(inputStream).load();
+        }
+    }
+}
+
+//http://www.ancestralauthor.com/download/sample.pdf
